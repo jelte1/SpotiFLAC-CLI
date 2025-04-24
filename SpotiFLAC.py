63211c726bacb9ee56444add36e5f6fc5c50ea1b
afkarxyz <mzamzamafkarhadiq@gmail.com>
2025-04-25 06:30:14 +0700

    v2.5

--- SpotiFLAC.py
+++ SpotiFLAC.py
@@ -330,7 +330,7 @@ class ServiceComboBox(QComboBox):
 class SpotiFLACGUI(QWidget):
     def __init__(self):
         super().__init__()
-        self.current_version = "2.4"
+        self.current_version = "2.5"
         self.tracks = []
         self.reset_state()
         
@@ -749,7 +749,7 @@ class SpotiFLACGUI(QWidget):
                 spacer = QSpacerItem(20, 6, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
                 about_layout.addItem(spacer)
 
-        footer_label = QLabel("v2.4 | April 2025")
+        footer_label = QLabel("v2.5 | April 2025")
         footer_label.setStyleSheet("font-size: 12px; color: palette(text); margin-top: 10px;")
         about_layout.addWidget(footer_label, alignment=Qt.AlignmentFlag.AlignCenter)
 
@@ -950,10 +950,21 @@ class SpotiFLACGUI(QWidget):
             self.followers_label.hide()
         
         if metadata.get('releaseDate'):
-            release_date = datetime.strptime(metadata['releaseDate'], "%Y-%m-%d")
-            formatted_date = release_date.strftime("%d-%m-%Y")
-            self.release_date_label.setText(f"<b>Released</b> {formatted_date}")
-            self.release_date_label.show()
+            try:
+                release_date = metadata['releaseDate']
+                if len(release_date) == 4:
+                    date_obj = datetime.strptime(release_date, "%Y")
+                elif len(release_date) == 7:
+                    date_obj = datetime.strptime(release_date, "%Y-%m")
+                else:
+                    date_obj = datetime.strptime(release_date, "%Y-%m-%d")
+                
+                formatted_date = date_obj.strftime("%d-%m-%Y")
+                self.release_date_label.setText(f"<b>Released</b> {formatted_date}")
+                self.release_date_label.show()
+            except ValueError:
+                self.release_date_label.setText(f"<b>Released</b> {metadata['releaseDate']}")
+                self.release_date_label.show()
         else:
             self.release_date_label.hide()
         
